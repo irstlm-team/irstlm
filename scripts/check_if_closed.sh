e300d1bae1827d9f0ce7cf82f349260c97262595
Nicola Bertoldi <nicolabertoldi@Nicolas-MacBook-Pro.local>
2016-03-08 08:53:25 +0100

    minor change

--- scripts/check_if_closed.sh
+++ scripts/check_if_closed.sh
@@ -11,7 +11,7 @@ err_str=$(lsof $file 2>&1 > /dev/null ; echo $?)
 until [ $err_str != 0 ]; do
   echo "file ($file) is still open" >&2 
   # lsof returned 1 but didn't print an error string, assume the file is open
-  sleep 1
+  sleep 1.0
   err_str=$(lsof $file 2>&1 >/dev/null ; echo $?)
 done
 echo "file ($file) is closed" >&2 
