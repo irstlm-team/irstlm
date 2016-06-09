10d312ecf8c7e2ce1affd7131f91b5c89b1a3f45
Nicola Bertoldi <nicolabertoldi@Nicolas-MacBook-Pro.local>
2016-06-09 08:15:05 +0200

    removed temporary compilation files from the repo

--- src/CMakeFiles/LinkToLib64.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/LinkToLib64"
-  "LNS"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/LinkToLib64.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- src/CMakeFiles/irstlm.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "../inst/lib/libirstlm.a"
-)
